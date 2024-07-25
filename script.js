f1509c948bd336542b7a964f504304124c5b11e0
unknown <chmueedzafar@gmail.com>
2024-07-25 17:04:51 +0500

    promises pratice

--- script.js
+++ script.js
@@ -67,4 +67,73 @@ function secondfunc(fullName,callback){
 }
 secondfunc("Abdul Mueed", greet)
 
+// Promises
+
+let psromise = new Promise (function (resolve, reject) {
+
+    let x = 20;
+    if (x > 10){
+        resolve (x);        
+    } else { 
+        reject ("Too low");     
+    }
+});
+
+Promise.then(
+    function (value){
+        console.log("sucess:", value);
+    },
+    function (error){
+        console.log("Error:", error);
+    }
+);
+
+const Promise = new Promise ((resolve, reject) =>{
+    resolve ("sucess!");
+})
+.then(value => {
+    console.log(value)
+    return "we"
+})
+.then(value => {
+    console.log(value)
+    return "can"
+})
+.then(value => {
+    console.log(value)
+    return "chain"
+})
+.then(value => {
+    console.log(value)
+    return "promises"
+})
+.then(value => {
+    console.log(value)
+})
+.catch (value => {
+    console.log(value);
+})
+
+const psromise1 = new psromise ((resolve, reject) => {
+    reject ("oops..");
+})
+.then (value => {
+    console.log(value);
+    return "we";
+})
+.then (value => {
+    console.log(value);
+    return "can";
+})
+.then (value => {
+    console.log(value);
+    return "chain";
+})
+.then (value => {
+    console.log(value);
+    return "promises";
+})
+.catch (value => {
+    console.log(value);
+})
 
